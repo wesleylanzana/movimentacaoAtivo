cc55e885135898d388ddb3126995773c64665005
Wesley Lanzana <wesley.lanzana@gmail.com>
2024-06-27 18:02:38 -0300

    Altração de Valor Unitário com lib maskMoney

---
+++ forms/movimentacaoAtivo/maskedMoney.js
@@ -0,0 +1,19 @@
+//continuar selecionando primeiro todos os campos pai e filho do Valor Unitário, utilizar um each ou forEach.
+
+
+
+/*$(document).ready(function() {
+    // Inicializa a máscara financeira no campo "valor" com o símbolo R$
+
+    var campoFilhoValorUnitario = $("input[id^='valorUnitario___']").val(); //responsável por acessar todos os campos de Valor Unitário da tabela pai e filho
+
+    console.log(campoFilhoValorUnitario);
+
+    $(campoFilhoValorUnitario).maskMoney({
+        prefix: 'R$ ',
+        allowNegative: false,
+        thousands: '.',
+        decimal: ',',
+        affixesStay: true
+    });
+});*/
